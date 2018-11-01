09bd241702e78a1254bc341739f4004b6d9760ac
philmtl <39309102+philmtl@users.noreply.github.com>
2018-11-01 11:36:03 -0400

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,17 @@
+num1 = float(input("enter first number: "))
+op = input("enter operator(*,/,+,-): ")
+num2 = float(input("enter 2nd number: "))
+
+if op == "+":
+    print(num1 + num   )
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("invalid oporator")
+
+input("press enter to close")
+
